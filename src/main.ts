6b1d7ae8099ad2401e01618b4b923b4b95f30d23
Erik Michelson <github@erik.michelson.eu>
2021-06-08 23:46:00 +0200

    Add adapter draft
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ import { AuthConfig } from './config/auth.config';
 import { MediaConfig } from './config/media.config';
 import { ConsoleLoggerService } from './logger/console-logger.service';
 import { BackendType } from './media/backends/backend-type.enum';
+import { WsAdapter } from '@nestjs/platform-ws';
 import { setupFrontendProxy } from './utils/frontend-integration';
 import { setupSessionMiddleware } from './utils/session';
 import { setupValidationPipe } from './utils/setup-pipes';
@@ -25,7 +26,6 @@ async function bootstrap(): Promise<void> {
     logger: ['error', 'warn', 'log'] as LogLevel[],
     bufferLogs: true,
   });
-  app.useWebSocketAdapter(new WsAdapter(app));
   const logger = await app.resolve(ConsoleLoggerService);
   logger.log('Switching logger', 'AppBootstrap');
   app.useLogger(logger);
@@ -33,6 +33,8 @@ async function bootstrap(): Promise<void> {
   const appConfig = configService.get<AppConfig>('appConfig');
   const authConfig = configService.get<AuthConfig>('authConfig');
   const mediaConfig = configService.get<MediaConfig>('mediaConfig');
+
+  app.useWebSocketAdapter(new WsAdapter(app));
 
   if (!appConfig || !authConfig || !mediaConfig) {
     logger.error('Could not initialize config, aborting.', 'AppBootstrap');
